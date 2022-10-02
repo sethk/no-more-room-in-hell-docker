2caca6eb6ead1d56ffe2e2e74004fa614f41736b
Seth Kingsley <seth@adhocla.com>
2022-10-01 19:05:53 -0700

    Bash is no longer installed, so use sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-set -e
-trap '' TERM INT HUP
+#!/bin/sh
 
-./nmrih/srcds_run -game nmrih -maxplayers 12 +map nmo_broadway +exec server.cfg +hostname "$SRV_HOSTNAME" +sv_password "$SRV_PASSWORD" +rcon_password "$RCON_PASSWORD"
+exec nmrih/srcds_run -game nmrih -maxplayers 12 +map nmo_broadway +exec server.cfg +hostname "$SRV_HOSTNAME" +sv_password "$SRV_PASSWORD" +rcon_password "$RCON_PASSWORD"
